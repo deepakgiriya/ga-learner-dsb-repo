0262fd43791152ef391999447bf20b041e88d1b3
Deepak Giriya <deepakgiriya@gmail.com>
2020-05-01 14:49:21 +0530

    Update code.py

--- Loan_Approval_Analysis/code.py
+++ Loan_Approval_Analysis/code.py
@@ -11,6 +11,7 @@ categorical_var = bank.select_dtypes(include = 'object')
 print(categorical_var)
 numerical_var = bank.select_dtypes(include = 'number')
 print(numerical_var)
+
 # code ends here
 
 
@@ -21,6 +22,7 @@ print(banks.isnull().sum())
 bank_mode = banks.mode().iloc[0]
 banks.fillna(bank_mode, inplace = True)
 print(banks.isnull().sum())
+
 #code ends here
 
 
@@ -28,7 +30,6 @@ print(banks.isnull().sum())
 # Code starts here
 avg_loan_amount = pd.pivot_table(banks,index=['Gender','Married','Self_Employed'],values='LoanAmount',aggfunc='mean')
 print(avg_loan_amount)
-
 
 # code ends here
 
@@ -63,6 +64,7 @@ print (percentage_nse)
 loan_term = banks['Loan_Amount_Term'].apply(lambda x: int(x)/12)
 big_loan_term = len(loan_term[loan_term>=25])
 print(big_loan_term)
+
 # code ends here
 
 
@@ -70,8 +72,7 @@ print(big_loan_term)
 # code starts here
 loan_groupby = banks.groupby('Loan_Status')[['ApplicantIncome', 'Credit_History']]
 mean_values = loan_groupby.mean()
-
-
+print(mean_values)
 
 # code ends here
 
